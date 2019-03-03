69824bd821a6bd3ed3edaff52a7bb36dcecf0c2a
m5r <mokht@rmi.al>
2019-03-03 22:27:36 +0100

    signé Raba

--- TD4/src/Explosives.java
+++ TD4/src/Explosives.java
@@ -1,5 +1,8 @@
 // Based on a B specification from Marie-Laure Potet.
 
+import java.util.Map;
+import java.util.HashMap;
+import java.util.ArrayList;
 
 // GROUPE 14
 
@@ -14,7 +17,7 @@ public class Explosives {
       @ (0 <= nb_inc && nb_inc < 50);
       @*/
 
-    // Cette propriété assure qu'il n'y aura pas plus de 30 éléments affectés à un batiment.
+    // Cette propriété assure qu'il n'y aura pas plus de 30 éléments affectés aux batiments.
     /*@ public invariant // Prop 2
       @ (0 <= nb_assign && nb_assign < 30);
       @*/
@@ -56,9 +59,34 @@ public class Explosives {
       @        (i != j && (assign[i][0]).equals(assign [j][0])) ==> // B = Bi = Bj
       @        (\forall int k; 0 <= k && k < nb_inc; // pour une paire de produits incompatibles (Pk0, Pk1)
       @           (!(assign[i][1]).equals(incomp[k][0]))    // Pi != Pk0 OU
-      @              10|| (!(assign[j][1]).equals(incomp[k][1]))))); // Pj != Pk1
+      @              || (!(assign[j][1]).equals(incomp[k][1]))))); // Pj != Pk1
       @*/
 
+    /*@ public invariant // Prop 8
+      @ (\forall int i; i > 0 && i < nb_assign;
+      @     (\forall int j; j > 0 && j < nb_assign;
+      @         (
+      @             i != j && (assign[i][0]).equals(assign [j][0])) ==>
+      @             (!assign[i][1].equals(assign[j][1])
+      @        )
+      @     )
+      @ );
+      @*/
+
+    /*@ public invariant // Prop 9
+	  @ (\forall int i; i >= 0 &&  i < nb_assign;
+      @     (\forall int j; j >= i && j < nb_assign;
+      @        (i != j && (assign[i][1]).equals(assign[j][1])) ==>
+      @		   (\forall int k; k >= j && k < nb_assign;
+      @		      (k != j && (assign[k][1]).equals(assign[j][1])) ==>
+      @		      !\exists int l; l >= 0 && l < nb_assign && assign[l][1].equals(assign[i][1]))));
+      @*/
+
+    /*@ public constraint // Prop 10
+	  @ (\old(nb_inc) <= nb_inc);
+	  @*/
+
+    //@ requires prod1.startsWith("Prod") && prod2.startsWith("Prod") && !prod1.equals(prod2) && nb_inc < 48;
     public void add_incomp(String prod1, String prod2) {
         incomp[nb_inc][0] = prod1;
         incomp[nb_inc][1] = prod2;
@@ -67,6 +95,13 @@ public class Explosives {
         nb_inc = nb_inc + 2;
     }
 
+    //@ requires bat.startsWith("Bat") && prod.startsWith("Prod") && nb_assign < 29;
+    /*@ requires
+      @ (\forall int i; i >= 0 && i < nb_assign;
+      @     assign[i][0] == bat && (\forall int j; j >= 0 && j < nb_inc;
+      @         !incomp[j][0].equals(prod) && !incomp[j][1].equals(assign[i][1])
+      @ ));
+      @*/
     public void add_assign(String bat, String prod) {
         assign[nb_assign][0] = bat;
         assign[nb_assign][1] = prod;
@@ -74,5 +109,44 @@ public class Explosives {
     }
 
     public void skip() {
+    }
+
+    public String findBat(String prod) {
+        ArrayList<String> incompatibilities = new ArrayList<>();
+        HashMap<String, ArrayList<String>> batiments = new HashMap<>();
+
+        for (int i = 0; i < nb_inc; i++) {
+            if (incomp[i][0] == prod) {
+                incompatibilities.add(incomp[i][1]);
+            }
+        }
+
+        for (int j = 0; j < nb_assign; j++) {
+            String batimentName = assign[j][0];
+            String productName = assign[j][1];
+            ArrayList<String> products = batiments.containsKey(batimentName) ? batiments.get(batimentName) : new ArrayList<>();
+            products.add(productName);
+            batiments.put(batimentName, products);
+        }
+
+        String batiment = "Bat_rien";
+        for (Map.Entry<String, ArrayList<String>> entry : batiments.entrySet()) {
+            String potential_batiment = entry.getKey();
+            ArrayList<String> products = entry.getValue();
+
+            ArrayList<String> filteredProducts = new ArrayList<>();
+            for (int i = 0; i < products.size(); i++) {
+                String product = products.get(i);
+                if (incompatibilities.contains(product)) {
+                  filteredProducts.add(product);
+                }
+            }
+
+            if (filteredProducts.size() > 0 && !filteredProducts.contains(prod)) {
+                batiment = potential_batiment;
+            }
+        }
+
+        return batiment;
     }
 }
--- TD4/src/TestExplosivesJUnit4.java
+++ TD4/src/TestExplosivesJUnit4.java
@@ -18,14 +18,14 @@ public class TestExplosivesJUnit4 {
     }
 
 
-    private void handleJMLAssertionError(JmlAssertionError e) {
-        if (e.getClass().equals(JmlAssertionError.PreconditionEntry.class)) {
-            System.out.println("\n INCONCLUSIVE " + (new Exception().getStackTrace()[1].getMethodName()) + "\n\t " + e.getMessage());
+    private void handleJMLAssertionError(JmlAssertionError err) {
+        if (err.getClass().equals(JmlAssertionError.PreconditionEntry.class)) {
+            System.out.println("\n INCONCLUSIVE " + (new Exception().getStackTrace()[1].getMethodName()) + "\n\t " + err.getMessage());
             nb_inconclusive++;
         } else {
             // test failure
             nb_fail++;
-            fail("\n\t" + e.getMessage());
+            fail("\n\t" + err.getMessage());
         }
     }
 
@@ -43,19 +43,109 @@ public class TestExplosivesJUnit4 {
     }
 
     @Test
-    public void testSequence_0() {
+    public void test_prop_1() {
         try {
             e = new Explosives();
-            e.add_incomp("Prod_Nitro", "Prod_Glycerine");
-            e.add_incomp("Prod_Dyna", "Prod_Mite");
-            e.add_assign("Bat_1", "Prod_Dyna");
-            e.add_assign("Bat_1", "Prod_Nitro");
-            e.add_assign("Bat_2", "Prod_Mite");
-            e.add_assign("Bat_1", "Prod_Glycerine");
-        } catch (JmlAssertionError e) {
-            handleJMLAssertionError(e);
+            for (int i = 0; i < 26; i++) {
+                e.add_incomp("Prod_Nitro", "Prod_Glycerine");
+            }
+        } catch (JmlAssertionError err) {
+            handleJMLAssertionError(err);
         }
     }
 
+    @Test
+    public void test_prop_2() {
+        try {
+            e = new Explosives();
+            e.add_incomp("Prod_Nitro", "Prod_Glycerine");
+            for (int i = 0; i < 31; i++) {
+                String prod = "Prod_Nitro_" + i;
+                e.add_assign("Bat_1", prod);
+            }
+        } catch (JmlAssertionError err) {
+            handleJMLAssertionError(err);
+        }
+    }
 
+    @Test
+    public void test_prop_3() {
+        try {
+            e = new Explosives();
+            e.add_incomp("Pas_Prod_Nitro", "Pas_Prod_Glycerine");
+        } catch (JmlAssertionError err) {
+            handleJMLAssertionError(err);
+        }
+    }
+
+    @Test
+    public void test_prop_4() {
+        try {
+            e = new Explosives();
+            e.add_incomp("Prod_Nitro", "Prod_Glycerine");
+            e.add_assign("Pas_Bat_1", "Prod_Nitro");
+        } catch (JmlAssertionError err) {
+            handleJMLAssertionError(err);
+        }
+    }
+
+    @Test
+    public void test_prop_5() {
+        try {
+            e = new Explosives();
+            e.add_incomp("Prod_Nitro", "Prod_Nitro");
+        } catch (JmlAssertionError err) {
+            handleJMLAssertionError(err);
+        }
+    }
+
+    @Test
+    public void test_prop_6() {
+        try {
+            e = new Explosives();
+            e.incomp[e.nb_inc][0] = "Prod_A";
+            e.incomp[e.nb_inc][1] = "Prod_B";
+            e.nb_inc = e.nb_inc + 1;
+            e.skip();
+        } catch (JmlAssertionError err) {
+            handleJMLAssertionError(err);
+        }
+    }
+
+    @Test
+    public void test_prop_6_bis() {
+        try {
+            e = new Explosives();
+            e.add_incomp("Prod_Nitro", "Prod_Glycerine");
+            assertEquals(e.incomp[0][0], e.incomp[1][1]);
+            assertEquals(e.incomp[0][1], e.incomp[1][0]);
+        } catch (JmlAssertionError err) {
+            handleJMLAssertionError(err);
+        }
+    }
+
+    @Test
+    public void test_prop_7() {
+        try {
+            e = new Explosives();
+            e.add_incomp("Prod_Nitro", "Prod_Glycerine");
+            e.add_assign("Bat_1", "Prod_Nitro");
+            e.add_assign("Bat_1", "Prod_Glycerine");
+        } catch (JmlAssertionError err) {
+            handleJMLAssertionError(err);
+        }
+    }
+
+    @Test
+    public void test_find_bat() {
+        try {
+            e = new Explosives();
+            e.add_assign("Bat_1", "Prod_Nitro");
+
+            String bat = e.findBat("Prod_Nitro");
+            assertEquals("Bat_rien", "Bat_rien");
+        } catch (JmlAssertionError err) {
+            handleJMLAssertionError(err);
+        }
+    }
 }
